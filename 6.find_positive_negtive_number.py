8117ff3627868de2049eca4c27e90ee1ec1e2245
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-14 17:30:22 +0530

    Find the number is Positive or Negtive

---
+++ 6.find_positive_negtive_number.py
@@ -0,0 +1,10 @@
+num = int(input("Enter Psitive or Negtive number: "))
+
+if num == 0:
+    print(f"Number {num} is zero.")
+    
+elif num > 0:
+    print(f"Number {num} is Positive.")
+    
+else:
+    print(f"Number {num} is Negative.")
