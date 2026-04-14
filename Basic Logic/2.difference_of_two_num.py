269cbb394eb9022f6576da819eb447b3f35fc33a
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-14 16:33:15 +0530

    Difference of two num

---
+++ Basic Logic/2.difference_of_two_num.py
@@ -0,0 +1,6 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+difference = float(num1) - float(num2)  
+
+print("The difference of", num1, "and", num2, "is:", difference)   
