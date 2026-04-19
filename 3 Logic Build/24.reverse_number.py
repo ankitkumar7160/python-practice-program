f68f12be64a4c32c2ae9f26f3626f1fb93234fcf
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-19 23:03:07 +0530

    Reverse Number

---
+++ 3 Logic Build/24.reverse_number.py
@@ -0,0 +1,12 @@
+# Reverse Number
+
+num = int(input("Enter the number: "))
+
+reverse_number = 0
+
+while num > 0:
+    last_digit = num % 10
+    reverse_number = (reverse_number*10)+last_digit
+    num = num // 10
+    
+print(reverse_number)
