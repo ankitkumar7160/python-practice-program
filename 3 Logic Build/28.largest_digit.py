36c368909f8ed456a1c279d53b1b50d5c3687f4e
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-20 22:53:54 +0530

    Find Largest digit

---
+++ 3 Logic Build/28.largest_digit.py
@@ -0,0 +1,12 @@
+num = int(input("Enter the number: "))
+
+temp = num
+max_digit = 0
+
+while temp > 0:
+    digit = temp % 10
+    if digit > max_digit:
+        max_digit = digit
+    temp //= 10
+
+print(f"The Largest digit in {num} is: {max_digit}")
