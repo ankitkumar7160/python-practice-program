a713e0e552b50769b94a05d46d2e22202aefd4fc
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-22 22:33:32 +0530

    Find Duplicate number in List

---
+++ 4 List_Array_Practice/36.find_duplicate_no_in_list.py
@@ -0,0 +1,18 @@
+n = int(input("Enter the length of array: "))
+array = []
+
+for i in range(n):
+    i = int(input("Enter Value: "))
+    array.append(i)
+print("Array: ", array)
+
+if len(array) == 0:
+    print("The array is empty")
+else:
+    duplicate = []
+    for x in range(len(array)):
+        for j in range(x+1, len(array)):
+            if array[x] == array[j]:
+                duplicate.append(array[x])
+                
+print(duplicate)
