c59bd0fb7c176798f0f1751fd0de8fe051ac6ac5
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-22 22:53:36 +0530

    Find Even numbers in List

---
+++ 4 List_Array_Practice/37.find_even_in_list.py
@@ -0,0 +1,18 @@
+n = int(input("Enter the Length of array: "))
+array = []
+
+for i in range(n):
+    i = int(input("Enter value: "))
+    array.append(i)
+
+print(f"Array: {array}")
+
+if len(array) == 0:
+    print("The array is empty")
+else:
+    even = []
+    for x in array:
+        if x % 2 == 0:
+            even.append(x)
+print(f"Even Number: {even}")
+            
