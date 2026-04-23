3b47eec8d3cc32e3fd61aa72695cb67b02b0f1a1
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-23 22:58:36 +0530

    Check Armstrong number

--- 3 Logic Build/27.check_armstrong_number.py
+++ 3 Logic Build/27.check_armstrong_number.py
@@ -1,14 +1,18 @@
 num = int(input("Enter the number: "))
 
+# Count number of digits
 order = len(str(num))
+
 temp_num = num
 total = 0
 
+# Calculate sum of digits raised to power of order
 while temp_num > 0:
     digit = temp_num % 10
     total += digit ** order
     temp_num //= 10
-    
+
+# Check Armstrong condition
 if num == total:
     print(f"{num} is armstrong number.")
 else:
