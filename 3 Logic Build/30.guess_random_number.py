375733e21c5de245073136a1d228872d31463152
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-23 22:51:32 +0530

    Number Guessing game

--- 3 Logic Build/30.guess_random_number.py
+++ 3 Logic Build/30.guess_random_number.py
@@ -1,24 +1,33 @@
 import random
 
+# Computer ek random number generate karega 1 se 100 ke beech
 computer = random.randint(1, 100)
+
+# User ke attempts count karne ke liye variable
 attempts = 1
 
 print("Random Number Guessing Game")
 
+# Infinite loop jab tak user sahi guess na kare
 while True:
+    
     user = int(input(f"Guess the number between 1 and 100 attempts {attempts}: "))
+    
+    # Har guess ke baad attempt increase hoga
     attempts += 1
     
     if user < computer:
-        print(f"Too Low! Try again")
-        
+        print("Too Low! Try again")
+
     elif user > computer:
-        print(f"Too High! Try again")
+        print("Too High! Try again")
     
     elif user == computer:
-        print(f"Congratulation: You Win🎉")
+        print("Congratulation: You Win🎉")
         break
+    
     else:
-        print(f"You Loss!🥺")
-        
-    # print(f"Your number of attempts {attempts}")
+        print("You Loss!🥺")
+
+# Total attempts dekhne ke liye niche wali line use kar sakte ho
+print(f"Your number of attempts {attempts}")
