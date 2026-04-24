26854a29a8e230d25a454db3b1e76125075e1bd5
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-24 23:25:47 +0530

    Find second Largest Number

---
+++ 4 List_Array_Practice/40.find_second_largest_number.py
@@ -0,0 +1,24 @@
+def find_second_largest(array):
+    if len(array) < 2:
+        return "This array is less than 2 values."
+    
+    largest = second_largest = float("-inf")
+    
+    for i in array:
+        if i > largest:
+            second_largest = largest
+            largest = i
+        
+        elif i > second_largest and i != largest:
+            second_largest = i
+    return second_largest
+
+n = int(input("Enter the Length of array: "))
+array = []
+
+for i in range(n):
+    i = int(input("Enter value: "))
+    array.append(i)
+
+print(f"Your original array {array}")
+print(f"Second largest number {find_second_largest(array)}")
