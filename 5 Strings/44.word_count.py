211dc7e7fc4bbaa328056ba88279f0fd4d1b3d2a
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-04-26 22:19:52 +0530

    Words Counting

---
+++ 5 Strings/44.word_count.py
@@ -0,0 +1,13 @@
+def word_count(sentence):
+    clean_sentence = sentence.strip()
+    
+    if not clean_sentence:
+        return 0
+    else:
+        new_sentence = clean_sentence.split()
+        
+    return len(new_sentence)
+
+sentence = input("Enter the sentence: ")
+result = word_count(sentence)
+print(f"{result} words in this sentence.")
