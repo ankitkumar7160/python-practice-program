c4fa6e16715aac04455c3bfa28a7f4b1ce48bf20
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-05-03 21:25:20 +0530

    Find Maximum Of Three Numbers

---
+++ 6 Function/51.find_maximum_of_three_num.py
@@ -0,0 +1,16 @@
+def maximum_number(number1, number2, number3):
+    
+    if number1 > number2 and number1 > number3:
+        return number1
+    elif number2 > number3 and number2 > number1:
+        return number2
+    else:
+        return number3
+
+number1 = int(input("Enter Number First: "))
+number2 = int(input("Enter Number Second: "))
+number3 = int(input("Enter Number Third: "))
+
+result = maximum_number(number1, number2, number3)
+
+print(result)
