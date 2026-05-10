1b375ce94e5f67c0ba2bf5f53a4d84cf2bb2488d
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-05-10 20:39:59 +0530

    Check Palindrome or Not

---
+++ 6 Function/56.palindrime.py
@@ -0,0 +1,32 @@
+# def palindrome(number):
+#     temp = number
+#     reversed_num = 0
+    
+#     while temp > 0:
+#         digit = temp % 10
+        
+#         reversed_num = (reversed_num * 10) + digit
+#         temp //= 10
+        
+#     return number == reversed_num
+
+
+# number = int(input("Enter the number: "))
+
+# if palindrome(number):
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+    
+def palindrome(text):
+    reversed_text = text[::-1]
+    
+    return text == reversed_text
+
+
+string = input("Enter the string: ")
+
+if palindrome(string):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
