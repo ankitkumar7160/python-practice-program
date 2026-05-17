a06f785770acc29cf47720fa85ade4df7e62bdd6
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-05-17 21:42:15 +0530

    Update Palindrome number

--- 5 Strings/42.check_palindrome_number.py
+++ 5 Strings/42.check_palindrome_number.py
@@ -1,20 +1,22 @@
 def palind(word):
     
-    word = word.lower()
+    word = word.lower()   # Convert word to lowercase for case-insensitive checking
     
-    left = 0
-    right = len(word)-1
-    is_palindrome = True
+    left = 0              # Starting index
+    right = len(word)-1   # Ending index
+    is_palindrome = True  # Assume word is palindrome initially
 
-    while left<right:
+    # Compare characters from both sides
+    while left < right:
         if word[left] != word[right]:
             is_palindrome = False
             break
         
-        left +=1
-        right -=1
+        left += 1
+        right -= 1
 
-    if is_palindrome :
+    # Print result
+    if is_palindrome:
         print("Word is palindrome.")
     else:
         print("Word is not palindrome.")
