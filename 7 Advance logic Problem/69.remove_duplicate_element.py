8706bbf6a24bc705fdd6d9007195e51d8d9fe28f
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-05-26 21:58:11 +0530

    Remove Duplicate Element

---
+++ 7 Advance logic Problem/69.remove_duplicate_element.py
@@ -0,0 +1,21 @@
+def remove_duplicate_element(my_array):
+    
+    uniqe_set = {}
+    
+    duplicate_list = []
+    
+    for num in my_array:
+        
+        if num not in uniqe_set:
+            duplicate_list.append(num)
+
+            uniqe_set[num] = True
+        
+    return duplicate_list
+
+my_input = input("Enter array values: ")
+
+new_array = [int(x) for x in my_input.split()]
+
+result = remove_duplicate_element(new_array)
+print(f"List after removing duplicates: {result}")
