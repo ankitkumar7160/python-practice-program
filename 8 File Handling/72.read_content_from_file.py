714dc6fc17546fd1d815079699efb8dc640337c8
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-05-30 21:52:20 +0530

    Read content from file

--- 8 File Handling/72.read_content_from_file.py
+++ 8 File Handling/72.read_content_from_file.py
@@ -1,10 +1,18 @@
 # Function to read content from a file
 
 def read_file():
-    with open("8 File Handling/Note.txt", "r") as file:
+
+    # File ko read mode me open karo
+    with open("Note.txt", "r") as file:
+
+        # File ka sara content read karo
         content = file.read()
 
+    # Success message print karo
     print("File content read successfully.")
+
+    # File ka content return karo
     return content
 
+# Function call karke content print karo
 print(read_file())
