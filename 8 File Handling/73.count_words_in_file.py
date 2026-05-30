49a76467e644d48701b6b266f727c292037654a4
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-05-30 21:49:35 +0530

    Add Comment for easy understanding

--- 8 File Handling/73.count_words_in_file.py
+++ 8 File Handling/73.count_words_in_file.py
@@ -2,11 +2,17 @@
 
 def count_word():
 
+    # File ko read mode me open karo
     with open("Note.txt", "r") as file:
+
+        # File ka sara content read karo
         content = file.read()
 
+    # Content ko words me divide karo
     words = content.split()
 
+    # Total words return karo
     return len(words)
 
+# Total words print karo
 print("Total words:", count_word())
