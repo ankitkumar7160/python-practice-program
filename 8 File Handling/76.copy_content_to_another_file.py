b003ff88f21dfc0b755b3b23028eea58284f44f2
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-06-02 22:51:02 +0530

    Copy content to another file

---
+++ 8 File Handling/76.copy_content_to_another_file.py
@@ -0,0 +1,13 @@
+def copy_content():
+    
+    with open("Note.txt", "r") as file:
+        content = file.read()
+        
+    n_content = content
+
+    with open("Copy_Note.txt","w") as n_file:
+        data = n_file.write(n_content)
+        
+    return "Content copy Successfully."
+
+print(copy_content)
