d931a8b75f704580dedd1135f360782929cfafc5
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-06-03 22:07:09 +0530

    Append text to file

---
+++ 8 File Handling/77.append_text_to_file.py
@@ -0,0 +1,10 @@
+def append_text(text):
+    
+    with open("Note.txt", "a") as file:
+        content = file.write("\n"+text)
+        
+    return "Append text is Successfully."
+
+text = input("Enter Data: ")
+
+print(append_text(text))
