0e917a129e3b68f3998ca844c02c2e857cc8591e
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-06-03 22:41:30 +0530

    Find most frequent word

---
+++ 8 File Handling/78.find_most_frequent_word.py
@@ -0,0 +1,20 @@
+def most_frequent():
+    
+    with open("Note.txt", "r") as file:
+        content = file.read()
+        
+    words = content.lower().replace(".","").split()
+    
+    word_map = {}
+    
+    for word in words:
+        if word in word_map:
+            word_map[word] += 1
+        else:
+            word_map[word] = 1
+            
+    most_frequent_word = max(word_map, key = word_map.get)
+    return most_frequent_word, word_map[most_frequent_word]
+
+word , count = most_frequent()
+print(f"Most frequent word is {word} appering {count} time.")
