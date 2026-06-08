bce364a19d50da1faca2591fc472897518cc4bc3
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-06-08 21:34:27 +0530

    Car Start

--- 9 Opps/84.car_class.py
+++ 9 Opps/84.car_class.py
@@ -16,3 +16,14 @@ class car():
             self.is_start = True
             # print("Car is Start.")
             return True
+    
+    def accelerate_car(self,increase):
+        if self.is_start == False:
+            print("Oops! Engine OFF.")
+            user = input("Do you want to Start Engine? yes/no: ").lower()
+            if user == "yes":
+                self.start_engine = self.start_engine()
+            else:
+                return False
+            
+        self.__speed = self.__speed + increase
