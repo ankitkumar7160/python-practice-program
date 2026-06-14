34d1bb9f5b6663600f02215af846d165af0377db
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-06-14 22:32:14 +0530

    Overriging

--- 9 Opps/87.overriging_method.py
+++ 9 Opps/87.overriging_method.py
@@ -19,4 +19,35 @@ class Animal():
             self.food = "Anithing"
         return
     
+    def speak(self):
+        return f"Animal Speak."
+    
+    def sleep(self):
+        return f"Animal is sleeping."
+    
+    def animal_details(self):
+        return f"""
+    
+    Animal Name : {self.animal_name}
+    Animal Species : {self.species}
+    Animal Age : {self.age}
+    Animal Gender : {self.gender}
+    Animal Eat : {self.eat()}
+    Animal Sleep : {self.sleep()}
+    Animal Speak : {self.speak()}
+    
+    """
+class dog(Animal):
+    
+    def __init__(self,name,species,age,gender):
+        super().__init__(name,species,age,gender)
+        
+    def speak(self):
+        parent_sound = super().speak()
+        return f"Barking: Woof Woof!"
+    
+    def animal_details(self):
+        all = super().animal_details()
+        return all
+
             
