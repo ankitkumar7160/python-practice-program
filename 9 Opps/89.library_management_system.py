d0ad05cc511c1664696e86173bfad33d182295da
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-06-27 22:29:29 +0530

    Library Management System

--- 9 Opps/89.library_management_system.py
+++ 9 Opps/89.library_management_system.py
@@ -6,14 +6,13 @@ class Book():
         self.author = author
         self.status = "Available"
         
-
     def __str__(self):
         return f"""
     Book ID : {self.book_ID}
     Book Title : {self.title}
     Author : {self.author}
-    """ 
-    
+    """
+
 class Member:
     
     def __init__(self,member_id,name):
@@ -45,7 +44,7 @@ class Library:
         return "Member Add Successfully."
     
     def issued_book(self,m_id,b_id):
-        if b_id in self.book and m_id in self.members:
+        if b_id in self.books and m_id in self.members:
             book = self.books[b_id]
             member = self.members[m_id]
             
@@ -58,6 +57,32 @@ class Library:
                 return "Error Book is already issued."
         else:
             return "Error: Invalid User ID or Book ID."
-    
-    
-                
+        
+    def return_book(self,m_id,b_id):
+        if b_id in self.books and m_id in self.members:
+            book = self.books[b_id]
+            member = self.members[m_id]
+            
+            if book in member.issued_book:
+                book_status = "Available"
+                member.issued_book.remove(book)
+                return f"Succes: {book.title} returned by {member.name}"
+            else:
+                return f"Erroe: This book was nt issued."
+        else:
+            return "Error: Record not found"
+        
+    def display_status(self):
+        print("\n === Current Library Status ===")
+        for b in self.books.values():
+            print(b)
+        print("-------------------------------\n")
+        
+my_lib = Library()
+
+my_lib.add_books(101,"Python","Guido van Rossum")
+my_lib.add_books(102,"Data Science","Jhon Smith")
+my_lib.add_member(11,'Akash')
+
+print(my_lib.issued_book(11,101))
+my_lib.display_status()
