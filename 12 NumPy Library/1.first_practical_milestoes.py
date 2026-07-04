27f8f9fcc0d06127536aba91b1346def8ff34f1c
ankitkumar7160 <ankitkumarsharma1020@gmail.com>
2026-07-04 22:08:58 +0530

    First practical

--- 12 NumPy Library/1.first_practical_milestoes.py
+++ 12 NumPy Library/1.first_practical_milestoes.py
@@ -4,3 +4,5 @@ import numpy as np
 arr = np.array([[1,2,3],[4,5,6]])
 print(f"Array {arr.ndim}D : \n{arr}")
 print(f"Dimension of Array : {arr.ndim}")
+print(f"Shape of array : {arr.shape}")
+print(f"Data type of array : {arr.dtype}")
